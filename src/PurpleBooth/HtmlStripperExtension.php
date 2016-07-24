b73e116bee6490b0e102068baffb401e32013b61
Billie Thompson <billie@purplebooth.co.uk>
2016-07-24 17:45:22 +0100

    Fixing scruitinizer issues
    Mostly adding PHPDoc and a few other things.

--- src/PurpleBooth/HtmlStripperExtension.php
+++ src/PurpleBooth/HtmlStripperExtension.php
@@ -2,30 +2,52 @@
 
 namespace PurpleBooth;
 
+/**
+ * This is sets up the twig extension
+ *
+ * @package PurpleBooth
+ */
 class HtmlStripperExtension extends \Twig_Extension
 {
+    /**
+     * This gets an array of the filters that this extension provides
+     *
+     * @return \Twig_SimpleFilter[]
+     */
     public function getFilters()
     {
-        return array(
-            new \Twig_SimpleFilter('html_strip', array($this, 'toText')),
-        );
+        return [
+            new \Twig_SimpleFilter('html_strip', [$this, 'toText']),
+        ];
     }
 
+    /**
+     * A convenient name for this extension
+     *
+     * @return string
+     */
     public function getName()
     {
         return "html_stripper";
     }
 
+    /**
+     * This is the extension itself, it's mostly a small wrapper around the actual parser.
+     *
+     * @param string $html
+     *
+     * @return string
+     */
     public function toText($html)
     {
         $parser = new Parser();
 
-        $xml_parser = xml_parser_create();
-        xml_set_element_handler($xml_parser, array($parser, 'startElement'), array($parser, 'endElement'));
-        xml_set_character_data_handler($xml_parser, array($parser, 'characterData'));
+        $xmlParser = xml_parser_create();
+        xml_set_element_handler($xmlParser, [$parser, 'startElement'], [$parser, 'endElement']);
+        xml_set_character_data_handler($xmlParser, [$parser, 'characterData']);
 
-        $wrappedHtml = "<root>$html</root>";
-        $returnStatus = xml_parse($xml_parser, $wrappedHtml, true);
+        $wrappedHtml  = "<root>$html</root>";
+        $returnStatus = xml_parse($xmlParser, $wrappedHtml, true);
 
         if (!$returnStatus) {
             return $html;
--- src/PurpleBooth/Parser.php
+++ src/PurpleBooth/Parser.php
@@ -2,22 +2,60 @@
 
 namespace PurpleBooth;
 
-
+/**
+ * This class is designed to by hooked into an XML parser to convert HTML to text.
+ *
+ * @see     xml_parser_create()
+ * @see     HtmlStripperExtension
+ *
+ * @package PurpleBooth
+ */
 class Parser
 {
 
+    /**
+     * @var string
+     */
     private $text = "";
+
+    /**
+     * @var \SplStack
+     */
     private $transformedTextStack;
+
+    /**
+     * @var \SplStack
+     */
     private $blockTypeStack;
+
+    /**
+     * @var \SplStack
+     */
     private $blockAttributesStack;
 
+    /**
+     * Parser constructor.
+     *
+     * Sets up the stacks
+     */
     public function __construct()
     {
         $this->transformedTextStack = new \SplStack();
-        $this->blockTypeStack = new \SplStack();
+        $this->blockTypeStack       = new \SplStack();
         $this->blockAttributesStack = new \SplStack();
     }
 
+    /**
+     * Function called on the start of an element
+     *
+     * Mostly used to prepend text to things, like the "*"s on LIs
+     *
+     * @see xml_set_element_handler()
+     *
+     * @param resource $parser
+     * @param string   $name
+     * @param array    $attrs
+     */
     public function startElement($parser, $name, $attrs)
     {
         $this->blockBegin($name, $attrs);
@@ -29,6 +67,64 @@ class Parser
         }
     }
 
+    /**
+     * Called when we begin a block.
+     *
+     * We build a series of stacks to represent the tree of the document
+     *
+     * We operate at this level of the stack when we're editing content
+     *
+     * @param string $name
+     * @param array  $attributes
+     */
+    private function blockBegin($name, $attributes)
+    {
+        $this->transformedTextStack->push("");
+        $this->blockTypeStack->push($name);
+        $this->blockAttributesStack->push($attributes);
+    }
+
+    /**
+     * Append some text to the current level of the stack
+     *
+     * @param string $value
+     */
+    private function appendBlockText($value)
+    {
+        $this->setBlockText($this->getBlockText() . $value);
+    }
+
+    /**
+     * Set the text for a block
+     *
+     * @param string $value
+     */
+    private function setBlockText($value)
+    {
+        $this->transformedTextStack->pop();
+        $this->transformedTextStack->push($value);
+    }
+
+    /**
+     * Get the current text that's in this block
+     *
+     * @return string
+     */
+    private function getBlockText()
+    {
+        return $this->transformedTextStack->top();
+    }
+
+    /**
+     * When we reach a closing element do something
+     *
+     * This is mostly used to add stuff to the end of a statement, like putting new lines where div tags close
+     *
+     * @see xml_set_element_handler()
+     *
+     * @param resource $parser
+     * @param string   $name
+     */
     public function endElement($parser, $name)
     {
         switch ($name) {
@@ -61,43 +157,45 @@ class Parser
         }
     }
 
+    /**
+     * Get the transformed text off the stack, and clear down the other stacks
+     *
+     * @return string
+     */
+    private function blockFinished()
+    {
+        $transformedText = $this->transformedTextStack->pop();
+        $this->blockTypeStack->pop();
+        $this->blockAttributesStack->pop();
+
+        return $transformedText;
+    }
+
+    /**
+     * This converts character data to human character data
+     *
+     * Primarily this is used for removing newlines and replacing them with spaces.
+     *
+     * @see xml_set_character_data_handler()
+     *
+     * @param resource $parser
+     * @param string   $data
+     */
     public function characterData($parser, $data)
     {
         $this->appendBlockText(str_replace("\n", " ", $data));
     }
 
-    public function getText() {
-        $text = trim($this->text);
+    /**
+     * This gets the text that has been parsed and returns it
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        $text  = trim($this->text);
         $lines = explode("\n", $text);
 
         return implode("\n", array_map("trim", $lines));
     }
-
-    private function appendBlockText($value) {
-        $this->setBlockText($this->getBlockText() . $value);
-    }
-
-    /**
-     * @param string $value
-     */
-    private function setBlockText($value) {
-        $this->transformedTextStack->pop();
-        $this->transformedTextStack->push($value);
-    }
-
-    private function getBlockText() {
-        return $this->transformedTextStack->top();
-    }
-
-    private function blockBegin($name, $attributes) {
-        $this->transformedTextStack->push("");
-        $this->blockTypeStack->push($name);
-        $this->blockAttributesStack->push($attributes);
-    }
-
-    private function blockFinished() {
-        return $this->transformedTextStack->pop();
-        return $this->blockTypeStack->pop();
-        return $this->blockAttributesStack->pop();
-    }
-} 
+}
